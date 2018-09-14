a0c23708248e4df623b1d4a12cafbf1222923b3e
Guido Flohr <guido.flohr@cantanea.com>
2018-09-14 17:01:04 +0300

    more boilerplate

---
+++ lib/Lingua/Poly/Word.pm
@@ -0,0 +1,36 @@
+# Lingua-Poly   Language Disassembling Library
+# Copyright (C) 2018 Guido Flohr <guido.flohr@cantanea.com>
+#               All rights reserved
+#
+# This library is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What the Fuck You Want
+# to Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
+
+package Lingua::Poly::Word;
+
+use strict;
+
+1;
+
+=head1 NAME
+
+Lingua::Poly::Word
+
+=head1 SYNOPSIS
+
+=head1 COPYRIGHT
+
+Copyright (C) 2018 Guido Flohr  <guido.flohr@cantanea.com>, all rights
+reserved.
+
+This library is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What the Fuck You Want
+to Public License, Version 2, as published by Sam Hocevar. See
+http://www.wtfpl.net/ for more details.
+
+=head1 SEE ALSO
+
+perl(1)
