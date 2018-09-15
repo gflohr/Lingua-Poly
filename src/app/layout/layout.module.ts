41513e7588686ebb89b4dca33dc514f2c84a79c5
Guido Flohr <guido.flohr@cantanea.com>
2018-09-15 13:07:51 +0300

    use bootstrap

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   exports: [
     HeaderComponent
