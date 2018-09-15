bccb0b2fbbede9990764847c3d8aefbb7f10643e
Guido Flohr <guido.flohr@cantanea.com>
2018-09-15 12:28:55 +0300

    core component

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { applicationConfig } from '../app.config';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class CoreModule { }
