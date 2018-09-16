9132ec7f874b7cef4415e784b661a734f68f1ee0
Guido Flohr <guido.flohr@cantanea.com>
2018-09-16 10:10:38 +0300

    separate sidebar component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CoreModule } from '../core/core.module';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    NgbModule
+    NgbModule,
+    CoreModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ],
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ]
 })
 export class LayoutModule { }
