caadd62c1a8bd0eee5d81ecc7555ae58b6127adf
Guido Flohr <guido.flohr@cantanea.com>
2018-09-16 14:27:27 +0300

    legal disclosure

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CoreModule } from '../core/core.module';
 import { mainRoutes }  from './main.routes';
 import { StartComponent } from './components/start/start.component';
 import { LegalDisclosureComponent } from './components/legal-disclosure/legal-disclosure.component';
@@ -8,7 +9,8 @@ import { LegalDisclosureComponent } from './components/legal-disclosure/legal-di
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(mainRoutes)
+    RouterModule.forChild(mainRoutes),
+    CoreModule
   ],
   declarations: [StartComponent, LegalDisclosureComponent]
 })
--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -1,5 +1,8 @@
 import { StartComponent } from './components/start/start.component';
+import { LegalDisclosureComponent } 
+    from './components/legal-disclosure/legal-disclosure.component';
 
 export const mainRoutes = [
-    { path: '', component: StartComponent }
+    { path: '', component: StartComponent },
+    { path: 'legal-disclosure', component: LegalDisclosureComponent },
 ];
