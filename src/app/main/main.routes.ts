80288e244665356bd0300f75091de34498f3f91d
Guido Flohr <guido.flohr@cantanea.com>
2018-09-16 15:01:07 +0300

    linked legal blurb

--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -1,8 +1,14 @@
 import { StartComponent } from './components/start/start.component';
 import { LegalDisclosureComponent } 
     from './components/legal-disclosure/legal-disclosure.component';
+import { DisclaimerComponent } 
+    from './components/disclaimer/disclaimer.component';
+import { DataPrivacyComponent } 
+    from './components/data-privacy/data-privacy.component';
 
 export const mainRoutes = [
     { path: '', component: StartComponent },
     { path: 'legal-disclosure', component: LegalDisclosureComponent },
+    { path: 'disclaimer', component: DisclaimerComponent },
+    { path: 'data-privacy', component: DataPrivacyComponent },
 ];
