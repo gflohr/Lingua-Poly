6af260afacbe015f33b96bd3ee0bef1333177afc
Guido Flohr <guido.flohr@cantanea.com>
2018-09-16 14:28:52 +0300

    restructure layout module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,9 +4,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { CoreModule } from '../core/core.module';
 
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   imports: [
