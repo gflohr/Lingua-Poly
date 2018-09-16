20650aa2d80f047c1fea5bcfb64dc2d635e5d5c7
Guido Flohr <guido.flohr@cantanea.com>
2018-09-16 11:55:22 +0300

    prepared legal disclosure

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { mainRoutes }  from './main.routes';
 import { StartComponent } from './components/start/start.component';
+import { LegalDisclosureComponent } from './components/legal-disclosure/legal-disclosure.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(mainRoutes)
   ],
-  declarations: [StartComponent]
+  declarations: [StartComponent, LegalDisclosureComponent]
 })
 export class MainModule { }
