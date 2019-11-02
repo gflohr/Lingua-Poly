f6d79965c0e5d96c52ab301aec38015064411f2f
Guido Flohr <guido.flohr@cantanea.com>
2019-11-02 10:03:41 +0200

    register component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { authRoutes } from './auth.routes';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(authRoutes)
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent, RegisterComponent]
 })
 export class AuthModule { }
--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,5 +1,7 @@
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const authRoutes = [
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
 ];
