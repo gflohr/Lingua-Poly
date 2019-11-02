c89d9400d3102eeb6a9f27fa6fbb1fc70cdb67c6
Guido Flohr <guido.flohr@cantanea.com>
2019-11-02 13:12:06 +0200

    username form control

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,12 +4,15 @@ import { RouterModule } from '@angular/router';
 import { authRoutes } from './auth.routes';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(authRoutes)
-  ],
-  declarations: [LoginComponent, RegisterComponent]
+	imports: [
+		CommonModule,
+		RouterModule.forChild(authRoutes),
+		FormsModule,
+		ReactiveFormsModule
+	],
+	declarations: [LoginComponent, RegisterComponent]
 })
 export class AuthModule { }
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+	username = new FormControl();
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
