89b2b95d2d06ce591a41f7ecbdc1e138a29e71f4
Guido Flohr <guido.flohr@cantanea.com>
2019-11-07 11:28:38 +0200

    unconfirmed users

--- schema.sql
+++ schema.sql
@@ -4,7 +4,8 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email TEXT NOT NULL UNIQUE,
     username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    confirmed BOOLEAN NOT NULL DEFAULT 'f'
 );
 INSERT INTO users(id, email, username, password) 
   SELECT 0, 'placeholder@example.com', 'admin', '{SHA512}c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec='
@@ -32,7 +33,7 @@ INSERT INTO user_groups (user_id, group_id)
 
 CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
-    sid CHAR(128) NOT NULL UNIQUE,
+    sid TEXT NOT NULL UNIQUE,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     footprint TEXT,
     last_seen TIMESTAMP WITH TIME ZONE DEFAULT NOW()
