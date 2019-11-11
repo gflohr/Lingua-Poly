fff984f9b691a2da223f00e4a88b9842b681a57e
Guido Flohr <guido.flohr@cantanea.com>
2019-11-11 11:57:01 +0200

    display registration page

---
+++ src/app/auth/components/registration-received/registration-received.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistrationReceivedComponent } from './registration-received.component';
+
+describe('RegistrationReceivedComponent', () => {
+  let component: RegistrationReceivedComponent;
+  let fixture: ComponentFixture<RegistrationReceivedComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RegistrationReceivedComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegistrationReceivedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/auth/components/registration-received/registration-received.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-registration-received',
+	templateUrl: './registration-received.component.html',
+	styleUrls: ['./registration-received.component.css']
+})
+export class RegistrationReceivedComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {
+	}
+
+}
