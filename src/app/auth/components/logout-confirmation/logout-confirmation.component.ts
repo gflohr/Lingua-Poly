3967d7b35ad4113c2a83d6a9b41333e8e75db83e
Guido Flohr <guido.flohr@cantanea.com>
2019-11-26 15:12:12 +0200

    logout confirmation component

---
+++ src/app/auth/components/logout-confirmation/logout-confirmation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logout-confirmation',
+  templateUrl: './logout-confirmation.component.html',
+  styleUrls: ['./logout-confirmation.component.css']
+})
+export class LogoutConfirmationComponent {
+
+	constructor() { }
+
+}
