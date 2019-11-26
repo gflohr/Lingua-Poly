3f62d28caee23522123b41b13bc24319c62a6307
Guido Flohr <guido.flohr@cantanea.com>
2019-11-26 16:18:35 +0200

    login and loginSuccess actions

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -3,24 +3,30 @@ import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { exhaustMap, map, catchError, tap } from 'rxjs/operators';
 import { UsersService } from '../core/openapi/lingua-poly';
 import { of } from 'rxjs';
+import { LoginPageActions, AuthApiActions } from './actions';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthEffects {
-	/*
 	login$ = createEffect(() => this.actions$.pipe(
 		ofType(LoginPageActions.login),
-			exhaustMap(action =>
-				this.usersService.userLogin(action.userLogin).pipe(
-					map(user => AuthApiActions.loginSuccess({ user: user })),
-					catchError(error => of(AuthApiActions.loginFailure({ error: error })))
-				)
+		map(action => action.credentials),
+		exhaustMap((userLogin) =>
+			this.usersService.userLogin(userLogin).pipe(
+				map(user => AuthApiActions.loginSuccess({ user })),
+				catchError(error => of(AuthApiActions.loginFailure({ error })))
 			)
 		)
-	)
-	*/
+	));
+
+	loginSuccess$ = createEffect(() => this.actions$.pipe(
+		ofType(AuthApiActions.loginSuccess),
+		tap(() => this.router.navigate(['/']))
+	), { dispatch: false });
 
 	constructor(
 		private actions$: Actions,
-		private usersService: UsersService
+		private usersService: UsersService,
+		private router: Router
 	) { }
 }
