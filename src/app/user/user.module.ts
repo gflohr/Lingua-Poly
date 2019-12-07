f998abf1f5a616026334fd8c0bd4412197230b47
Guido Flohr <guido.flohr@cantanea.com>
2019-12-07 12:21:51 +0200

    profile component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ProfileComponent]
 })
 export class UserModule { }
