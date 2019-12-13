850ef78af76104c3c5b6e68c125076fa34a7ea36
Guido Flohr <guido.flohr@cantanea.com>
2019-12-13 08:49:11 +0200

    load url checker service

---
+++ apis/um/lib/Lingua/Poly/API/UM/Service/URLChecker.pm
@@ -0,0 +1,25 @@
+#! /bin/false
+#
+# Lingua-Poly   Language Disassembling Library
+# Copyright (C) 2018-2019 Guido Flohr <guido.flohr@Lingua::Poly::API.com>
+#			   All rights reserved
+#
+# This library is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What the Fuck You Want
+# to Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
+
+package Lingua::Poly::API::UM::Service::URLChecker;
+
+use strict;
+
+sub new {
+        my ($class) = @_;
+        
+        my $self = '';
+        
+        bless \$self, $class;
+}
+
+1;
