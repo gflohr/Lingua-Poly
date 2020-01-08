d8cf8f818dfdd159fba7bd6dbf2f9d3901da1b3f
Guido Flohr <guido.flohr@cantanea.com>
2020-01-08 19:48:29 +0200

    fixed tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { LayoutModule } from './layout/layout.module';
+import { ROOT_REDUCERS, metaReducers } from './app.reducers';
+import { StoreModule } from '@ngrx/store';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			imports: [
-				RouterTestingModule
+				CoreModule,
+				LayoutModule,
+				RouterTestingModule,
+				StoreModule.forRoot(ROOT_REDUCERS, { metaReducers }),
 			],
 			declarations: [
 				AppComponent
@@ -18,18 +25,5 @@ describe('AppComponent', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.debugElement.componentInstance;
 		expect(app).toBeTruthy();
-	});
-
-	it(`should have as title 'Lingua-Poly'`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.debugElement.componentInstance;
-		expect(app.title).toEqual('Lingua-Poly');
-	});
-
-	it('should render title in a h1 tag', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector('h1').textContent).toContain('Welcome to Lingua-Poly!');
 	});
 });
