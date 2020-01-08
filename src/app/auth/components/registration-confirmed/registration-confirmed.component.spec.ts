e2a948713e410f23b83609f28188f3da3a60610e
Guido Flohr <guido.flohr@cantanea.com>
2020-01-08 20:01:39 +0200

    import RouterTestingModule

--- src/app/auth/components/registration-confirmed/registration-confirmed.component.spec.ts
+++ src/app/auth/components/registration-confirmed/registration-confirmed.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistrationConfirmedComponent } from './registration-confirmed.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RegistrationConfirmedComponent', () => {
 	let component: RegistrationConfirmedComponent;
@@ -8,6 +9,7 @@ describe('RegistrationConfirmedComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
+			imports: [RouterTestingModule],
 			declarations: [ RegistrationConfirmedComponent ]
 		})
 		.compileComponents();
