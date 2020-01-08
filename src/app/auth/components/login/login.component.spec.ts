5ced6d416c84ebe7ac58aa9e67b8db483696999f
Guido Flohr <guido.flohr@cantanea.com>
2020-01-08 20:25:59 +0200

    mock store

--- src/app/auth/components/login/login.component.spec.ts
+++ src/app/auth/components/login/login.component.spec.ts
@@ -2,17 +2,31 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { Store } from '@ngrx/store';
 
 describe('LoginComponent', () => {
 	let component: LoginComponent;
 	let fixture: ComponentFixture<LoginComponent>;
+	let store: MockStore<{}>;
+	const initialState = {};
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			imports: [ FormsModule, ReactiveFormsModule ],
-			declarations: [ LoginComponent ]
+			imports: [
+				FormsModule,
+				ReactiveFormsModule,
+				TranslateModule.forRoot()
+			],
+			declarations: [ LoginComponent ],
+			providers: [
+				provideMockStore({ initialState })
+			]
 		})
 		.compileComponents();
+
+		store = TestBed.get<Store<any>>(Store);
 	}));
 
 	beforeEach(() => {
