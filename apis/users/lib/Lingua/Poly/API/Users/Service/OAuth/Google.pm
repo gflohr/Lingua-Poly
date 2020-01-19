98066395500256040c4807aa3ab086f5d4c37cc1
Guido Flohr <guido.flohr@cantanea.com>
2020-01-19 15:32:53 +0200

    google oauth service skeleton

---
+++ apis/users/lib/Lingua/Poly/API/Users/Service/OAuth/Google.pm
@@ -0,0 +1,27 @@
+#! /bin/false
+#
+# Lingua-Poly   Language Disassembling Library
+# Copyright (C) 2018-2019 Guido Flohr <guido.flohr@Lingua::Poly::API.com>
+#			   All rights reserved
+#
+# This library is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What the Fuck You Want
+# to Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
+
+package Lingua::Poly::API::Users::Service::OAuth::Google;
+
+use strict;
+
+use Moose;
+use namespace::autoclean;
+
+use base qw(Lingua::Poly::API::Users::Logging);
+
+has logger => (is => 'ro', required => 1);
+has configuration => (id => 'ro', required  => 1);
+
+__PACKAGE__->meta->make_immutable;
+
+1;
