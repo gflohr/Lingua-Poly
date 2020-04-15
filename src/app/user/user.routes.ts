d78e7cbb8b4f82978301dabe4a28aa9617f086c4
Guido Flohr <guido.flohr@cantanea.com>
2020-04-15 17:07:32 +0300

    change password link

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,6 +1,16 @@
 import { marker as _ } from '@biesbjerg/ngx-translate-extract-marker';
 import { ProfileComponent } from './components/profile/profile.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
 
 export const userRoutes = [
-	{ path: 'profile', component: ProfileComponent, data: { title: _('Profile') } },
+	{
+		path: 'profile',
+		component: ProfileComponent,
+		data: { title: _('Profile') }
+	},
+	{
+		path: 'change-password',
+		component: ChangePasswordComponent,
+		data: { title: _('Change Password') }
+	},
 ];
