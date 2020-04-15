2111180be22809337cd1a35d40c67a1c9a909ff5
Guido Flohr <guido.flohr@cantanea.com>
2020-04-15 17:43:15 +0300

    change password form

--- src/app/user/components/change-password/change-password.component.ts
+++ src/app/user/components/change-password/change-password.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from '../../../core/openapi/lingua-poly';
+import { PasswordValidator } from '../../../core/validators/passwordValidator';
 
 @Component({
-  selector: 'app-change-password',
-  templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.sass']
+	selector: 'app-change-password',
+	templateUrl: './change-password.component.html',
+	styleUrls: ['./change-password.component.css']
 })
-export class ChangePasswordComponent implements OnInit {
+export class ChangePasswordComponent {
+	constructor(private fb: FormBuilder,
+		private router: Router,
+		private usersService: UsersService) { }
 
-  constructor() { }
+		changePasswordForm = this.fb.group ({
+			email: [ null ],
+			oldPassword: ['', [Validators.required ]],
+			password: ['', Validators.required],
+			passwordStrength: [ null ],
+			password2: ['', Validators.required ]
+		}, {
+			validators:
+				[
+					PasswordValidator.passwordMatch,
+					PasswordValidator.passwordStrength
+				]
+			}
+		);
 
-  ngOnInit() {
-  }
+	onSubmit() {
+		//const user = {
+		//	email: this.registrationForm.get('email').value,
+		//	password: this.registrationForm.get('password').value,
+		//} as UserDraft;
+		//this.usersService.usersPost(user).subscribe(() =>
+		//	this.router.navigate(['../registration/received', user.email])
+		//);
+	}
 
+	get oldPassword() { return this.changePasswordForm.get('oldPassword'); }
+	get password() { return this.changePasswordForm.get('password'); }
+	get password2() { return this.changePasswordForm.get('password'); }
 }
