f701d1208d6c9353de5b3f5d4db944ac02d57507
Guido Flohr <guido.flohr@cantanea.com>
2020-04-16 15:26:30 +0300

    reset password component

--- src/app/core/actions/user.actions.ts
+++ src/app/core/actions/user.actions.ts
@@ -3,12 +3,18 @@ import { Profile, PasswordChange } from '../openapi/lingua-poly';
 
 export const idleTimeout = createAction('[User] Idle Timeout');
 export const requestProfile = createAction('[Navigation] Request Profile');
+
 export const setProfile = createAction(
 	'[Profile Page] Set Profile',
 	props<{ payload: Profile}>()
 );
+
 export const changePassword = createAction(
 	'[Change Password Page] Change Password',
 	props<{ payload: PasswordChange}>()
 );
-export const dummyAction = createAction('[User] FIXME');
+
+export const resetPasswordRequest = createAction(
+	'[Reset Password Page] Reset Password',
+	props<{ payload: string}>()
+);
--- src/app/user/components/reset-password/reset-password.component.ts
+++ src/app/user/components/reset-password/reset-password.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import * as fromAuth from '../../../auth/reducers';
+import { Store } from '@ngrx/store';
+import { UserActions } from '../../../core/actions';
 
 @Component({
 	selector: 'app-reset-password',
 	templateUrl: './reset-password.component.html',
 	styleUrls: ['./reset-password.component.css']
 })
-export class ResetPasswordComponent implements OnInit {
+export class ResetPasswordComponent {
+	constructor(
+		private fb: FormBuilder,
+		private authStore: Store<fromAuth.State>,
+	) {}
 
-	constructor() { }
+	resetPasswordForm = this.fb.group({
+		id: ['', [ Validators.required ]],
+	});
 
-	ngOnInit() {
+	onSubmit() {
+		const id = this.resetPasswordForm.get('id').value;
+		this.authStore.dispatch(UserActions.resetPasswordRequest({ payload: id }));
 	}
-
 }
